7cdf83e920f89ad6492335fb7eeb85b423d05761
mateugazi <mateugazi182@outlook.com>
2021-03-26 00:11:46 +0100

    Changed mongoDB link and added environment variables

--- nestjs-intro/src/app.module.ts
+++ nestjs-intro/src/app.module.ts
@@ -1,15 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-
 @Module({
   imports: [
     ProductsModule,
     MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@nodeacademindrest.3i7s0.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@nodeacademindrest.3i7s0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     ),
   ],
   controllers: [AppController],
